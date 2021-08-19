d73c1317c7db7fdb58ad03f3807c1e8a2642f151
YouHojoon <dbghwns66@daum.net>
2021-08-19 17:00:15 +0900

    init CIN_IOS

---
+++ CIN_IOS/CIN/App/AppComponent.swift
@@ -0,0 +1,14 @@
+//
+//  AppComponet.swift
+//  CIN
+//
+//  Created by 유호준 on 2021/08/19.
+//
+
+import Foundation
+import RIBs
+class AppComponent: Component<EmptyComponent>, RootDependency {
+    init() {
+        super.init(dependency: EmptyComponent())
+    }
+}
---
+++ CIN_IOS/Pods/Target Support Files/Pods-CIN-CINUITests/Pods-CIN-CINUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CIN_CINUITests : NSObject
+@end
+@implementation PodsDummy_Pods_CIN_CINUITests
+@end
